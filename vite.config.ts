6e37a03cf5ee20e75f035d25af76f874334b6ee9
mimi489 <nnnrrrtttsss123@gmail.com>
2026-04-22 11:50:12 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -57,9 +57,7 @@ export default defineConfig(({mode}) => {
         }
       }
     },
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+  
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
